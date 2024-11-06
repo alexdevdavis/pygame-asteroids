457d25e03074dbe64318c5ae0aa7cfd63c60de42
alexdevdavis <alexdevdavis@gmail.com>
2024-11-06 14:09:21 +0000

    feat: redraw game canvas at maximum 60fps

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
 # start game loop
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
