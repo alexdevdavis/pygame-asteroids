95c0c139ddbd720a738c0bb5081af6e807f3529b
alexdevdavis <alexdevdavis@gmail.com>
2024-11-06 16:36:53 +0000

    feat: use Groups to manage game objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
@@ -9,17 +10,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 # start game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
